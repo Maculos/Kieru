ceff1da814db1f0dd49aee2ecf31e9787e3ff3db
Maculos <63527367+Maculos@users.noreply.github.com>
2021-11-09 04:42:24 -0600

    so, so, so many dumb mistakes that are gone now

--- modules/waitwhat.py
+++ modules/waitwhat.py
@@ -16,23 +16,36 @@ class MineStatus(Cog):
 	
 	@commands.command(name='ms', aliases=['minestatus', 'status'])
 	async def ms(self, ctx):
-		try:
-			status = self.server.status()
-			embed=Embed(title="Online", url="", description="{0} players online with a ping of {1} ms".format(status.players.online, status.latency), color=int(os.getenv('COLOR_SUCCESS'), 16))
-			await ctx.send(embed=embed)
-		except:
-			embed=Embed(title="Offline", url="", description="", color=int(os.getenv('COLOR_ERROR'), 16))
-			await ctx.send(embed=embed)
+		if ctx.guild.id == 801180663122100234:
+			try:
+				status = self.server.status()
+				embed=Embed(title="Online", url="", description="{0} players online with a ping of {1} ms".format(status.players.online, status.latency), color=int(os.getenv('COLOR_SUCCESS'), 16))
+				await ctx.send(embed=embed)
+			except:
+				embed=Embed(title="Offline", url="", description="", color=int(os.getenv('COLOR_ERROR'), 16))
+				await ctx.send(embed=embed)
+		else:
+			return
 	
 	@commands.command(name="ms.players", help="Gets the server's status.", aliases=["ms.p"])
 	async def players(self, ctx):
-		try:
+		if ctx.guild.id == 801180663122100234:
 			query = self.server.query()
 			embed = Embed(title="Online Players", description=f"{', '.join(query.players.names)}", color=int(os.getenv('COLOR_SUCCESS'), 16))
 			await ctx.send(embed=embed)
-		except:
-			embed = Embed(title="Offline", description="", color=int(os.getenv('COLOR_FAIL'), 16))
-			await ctx.send(embed=embed)
+		else:
+			await ctx.reply("This command is only available in the WaitWhat server.")
+	@Cog.listener()
+	async def on_message(self,ctx):
+		if ctx.author.bot:
+			return
+		else:
+			if ctx.author.guild_permissions.administrator: 
+				if ctx.mentions:
+					if ctx.mentions[0].id == 332287078832537601:
+						await ctx.reply("They'll be notified, including via threats if needed.")
+					else:
+						return
 
 def setup(bot):
 	bot.add_cog(MineStatus(bot))
